1b62bcd573e0bd6cbac46b40b3e23f159d13c434
Davy Hausser <davy.hausser@gmail.com>
2020-10-03 14:04:53 +0200

    :white_check_mark: 05: Styling

--- src/exercise/05.js
+++ src/exercise/05.js
@@ -4,29 +4,39 @@
 import React from 'react'
 import '../box-styles.css'
 
-function Box({ className, style, children }) {
+function Box({ className = '', style, ...otherProps }) {
+  console.log(className)
   return (
     <div
       className={`box ${className}`}
-      style={{ backgroundColor: style.backgroundColor }}
-    >
-      {children}
-    </div>
+      style={{ fontStyle: 'italic', ...style }}
+      {...otherProps}
+    />
   )
 }
+
+const smallBox = (
+  <Box className="box--small" style={{ backgroundColor: 'lightblue' }}>
+    small lightblue box
+  </Box>
+)
+const mediumBox = (
+  <Box className="box--medium" style={{ backgroundColor: 'pink' }}>
+    medium lightblue box
+  </Box>
+)
+const largeBox = (
+  <Box className="box--large" style={{ backgroundColor: 'orange' }}>
+    large lightblue box
+  </Box>
+)
 
 function App() {
   return (
     <div>
-      <Box className="box--small" style={{ backgroundColor: 'lightblue' }}>
-        small lightblue box
-      </Box>
-      <Box className="box--medium" style={{ backgroundColor: 'pink' }}>
-        small lightblue box
-      </Box>
-      <Box className="box--large" style={{ backgroundColor: 'orange' }}>
-        small lightblue box
-      </Box>
+      {smallBox}
+      {mediumBox}
+      {largeBox}
     </div>
   )
 }
